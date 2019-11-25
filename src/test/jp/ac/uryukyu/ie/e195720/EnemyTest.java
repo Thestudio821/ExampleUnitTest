e3bd803ec6dc219685281c1b7e3d2f3f8fbb46cb
Kazunari ENOKAWA <e195720@ie.u-ryukyu.ac.jp>
2019-11-25 17:37:57 +0900

    report3

---
+++ src/test/jp/ac/uryukyu/ie/e195720/EnemyTest.java
@@ -0,0 +1,26 @@
+package jp.ac.uryukyu.ie.e195720;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+    /**
+     * 「死んでる状態では攻撃できない」ことを確認!。
+     */
+    @Test
+    void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.setDead(true);
+        for(int i=0; i<10; i++) {
+            hero.attack(enemy); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.getHitPoint());
+    }
+
+    @Test
+    void testAttack() {
+    }
+}
